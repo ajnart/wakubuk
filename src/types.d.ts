bc53167accc92785ea693a937a85af96c494c7ab
ajnart <ajnart@pm.me>
2023-05-05 00:23:57 +0900

    🏷️ Update type definitions

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,15 @@
 interface Disk {
-		name: string,
-    mountPoint: String,
-    totalSpace: number,
-    availableSpace: number,
-    isRemovable: bool,
+  name: string;
+  mountPoint: String;
+  totalSpace: number;
+  availableSpace: number;
+  isRemovable: bool;
+}
+
+interface DiskRouteParams {
+  name: string;
+  path: string,
+  used: number;
+  isFullscan: boolean;
+  isDirectory: boolean;
 }
