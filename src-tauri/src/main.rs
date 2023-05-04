a544a761fec9f0f8e28041d14c2408e28c43b094
ajnart <ajnart@pm.me>
2023-05-05 00:23:42 +0900

    Add open_folder command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+
 use serde::Serialize;
 use sysinfo::{DiskExt, System, SystemExt};
 
@@ -35,13 +37,44 @@ fn get_disks() -> String {
             is_removable: disk.is_removable(),
         });
     }
-    // Return vec 
+    // Return vec
     serde_json::to_string(&vec).unwrap().into()
+}
+
+#[tauri::command]
+fn open_folder(path: String) {
+    #[cfg(target_os = "windows")]
+    {
+        let re = Regex::new(r"/").unwrap();
+        let result = re.replace_all(&path, "\\");
+        Command::new("explorer")
+            .args(["/select,", format!("{}", result).as_str()])
+            .spawn()
+            .unwrap();
+    }
+
+    #[cfg(target_os = "linux")]
+    {
+        let new_path = match metadata(&path).unwrap().is_dir() {
+            true => path,
+            false => {
+                let mut path2 = PathBuf::from(path);
+                path2.pop();
+                path2.into_os_string().into_string().unwrap()
+            }
+        };
+        Command::new("xdg-open").arg(&new_path).spawn().unwrap();
+    }
+
+    #[cfg(target_os = "macos")]
+    {
+        Command::new("open").args(["-R", &path]).spawn().unwrap();
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, get_disks])
+        .invoke_handler(tauri::generate_handler![greet, get_disks, open_folder])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
