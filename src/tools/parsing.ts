be55d955d24c063cf026ee192f7291164c777a40
ajnart <ajnart@pm.me>
2023-05-08 00:22:36 +0900

    ✨ Add format byte function

--- src/tools/parsing.ts
+++ src/tools/parsing.ts
@@ -1,5 +1,17 @@
-import { DataStructure } from "../types";
+import { DataStructure } from '../types';
 
 export function parseDataStructure(jsonString: string): DataStructure {
   return JSON.parse(jsonString) as DataStructure;
+}
+
+export function formatBytes(bytes: number): string {
+  const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
+  let magnitude = 0;
+
+  while (bytes >= 1024 && magnitude < units.length - 1) {
+    bytes /= 1024;
+    magnitude++;
+  }
+
+  return `${bytes.toFixed(1)} ${units[magnitude]}`;
 }
