a461d86576e4ee60f77f7e9e913b69862aa830c6
ajnart <ajnart@pm.me>
2023-05-07 19:56:56 +0900

    ✨ Add file tree component

---
+++ src/components/FileTreeDisplay.tsx
@@ -0,0 +1,22 @@
+import { Stack, Title, NavLink, Text } from '@mantine/core';
+import { IconFolder, IconFile } from '@tabler/icons-react';
+import { parseDataStructure } from '../tools/parsing';
+
+export function FileTreeDisplay({ status, name }: { status: any; name: string }) {
+  const data = parseDataStructure(status);
+  console.log({ data });
+  const renderTreeNodes = (nodes: TreeNode[]): React.ReactNode => {
+    return nodes.map((node) => (
+      <NavLink icon={node.children.length > 0 ? <IconFolder /> : <IconFile />} key={node.name} label={node.name}>
+        {node.children.length > 0 && renderTreeNodes(node.children)}
+      </NavLink>
+    ));
+  };
+
+  return (
+    <Stack style={{ height: '90vh' }}>
+      <Title>Disk: {name}</Title>
+      {renderTreeNodes(data.tree.children)}
+    </Stack>
+  );
+}
