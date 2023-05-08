a69267efc6fc4479fe3a7ad21101552d335c92bb
ajnart <ajnart@pm.me>
2023-05-08 12:00:18 +0900

    ✨ Add header and dark mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { ActionIcon, AppShell, ColorScheme, ColorSchemeProvider, Header, MantineProvider } from '@mantine/core';
 import { useColorScheme, useHotkeys } from '@mantine/hooks';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import IndexPage from './pages/IndexPage';
 import DiskPage from './pages/AnalyzePage';
 import { Notifications } from '@mantine/notifications';
+import { IconMoonStars, IconSun } from '@tabler/icons-react';
 
 export default function App() {
   const preferredColorScheme = useColorScheme('dark');
@@ -22,11 +23,31 @@ export default function App() {
     <MemoryRouter>
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
         <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-          <Notifications position="bottom-right" />
-          <Routes>
-            <Route path="/" element={<IndexPage />} />
-            <Route path="/analyze" element={<DiskPage />} />
-          </Routes>
+          <AppShell
+            header={
+              <Header fixed height={30} data-tauri-drag-region>
+                <div unselectable="on">
+                  <ActionIcon
+                    pos="absolute"
+                    right={5}
+                    top={2}
+                    variant="transparent"
+                    color={colorScheme === 'dark' ? 'yellow' : 'blue'}
+                    onClick={() => toggleColorScheme()}
+                    title="Toggle color scheme"
+                  >
+                    {colorScheme === 'dark' ? <IconSun size="1.1rem" /> : <IconMoonStars size="1.1rem" />}
+                  </ActionIcon>
+                </div>
+              </Header>
+            }
+          >
+            <Notifications position="bottom-right" />
+            <Routes>
+              <Route path="/" element={<IndexPage />} />
+              <Route path="/analyze" element={<DiskPage />} />
+            </Routes>
+          </AppShell>
         </MantineProvider>
       </ColorSchemeProvider>
     </MemoryRouter>
