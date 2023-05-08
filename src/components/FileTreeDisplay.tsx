43c0399c566c47a75e8f0cb2fd7b56526f5316b6
ajnart <ajnart@pm.me>
2023-05-08 12:47:46 +0900

    ✨ Add ability to open folder on right click

--- src/components/FileTreeDisplay.tsx
+++ src/components/FileTreeDisplay.tsx
@@ -1,25 +1,30 @@
-import { Stack, Title, NavLink, Text, ScrollArea, Group, Badge, Flex, Container } from '@mantine/core';
-import { IconFolder, IconFile } from '@tabler/icons-react';
+import { Stack, Title, NavLink, Text, ScrollArea, Group, Badge, Flex, Container, Menu } from '@mantine/core';
+import { IconFolder, IconFile, IconExternalLink } from '@tabler/icons-react';
 import { formatBytes, parseDataStructure } from '../tools/parsing';
-import { TreeNode } from '../types';
+import { DataStructure, TreeNode } from '../types';
+import { useState } from 'react';
+import { invoke } from '@tauri-apps/api';
 
-export function FileTreeDisplay({ status, name }: { status: any; name: string }) {
-  const data = parseDataStructure(status);
-  console.log(status);
-  const renderTreeNodes = (nodes: TreeNode[]): React.ReactNode => {
+export function FileTreeDisplay({ data, name }: { data: DataStructure; name: string }) {
+  const renderTreeNodes = (nodes: TreeNode[], currentPath: string): React.ReactNode => {
     return nodes.map((node) => (
       <NavLink
+        onContextMenuCapture={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          invoke('open_folder', { path: `${currentPath}/${node.name}`, inside: true });
+        }}
         icon={node.children.length > 0 ? <IconFolder /> : <IconFile />}
         key={node.name}
         disableRightSectionRotation
         rightSection={
-          <Badge color="blue" variant="light" size="xs">
+          <Badge color="blue" variant="outline" size="sm">
             {formatBytes(node.data)}
           </Badge>
         }
         label={<Group spacing={'xs'}>{node.name}</Group>}
       >
-        {node.children.length > 0 && renderTreeNodes(node.children)}
+        {node.children.length > 0 && renderTreeNodes(node.children, `${currentPath}/${node.name}`)}
       </NavLink>
     ));
   };
@@ -30,7 +35,7 @@ export function FileTreeDisplay({ status, name }: { status: any; name: string })
         height: '80vh',
       }}
     >
-      {renderTreeNodes(data.tree.children)}
+      {renderTreeNodes(data.tree.children, data.tree.name)}
     </ScrollArea>
   );
 }
